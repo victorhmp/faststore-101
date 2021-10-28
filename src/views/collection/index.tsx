dfb9f158143731f95af4c55021ec12757ce5b113
victorhmp <victor2142@gmail.com>
2021-10-28 17:06:59 -0300

    Update layout for Collection pages

--- src/views/collection/index.tsx
+++ src/views/collection/index.tsx
@@ -3,10 +3,13 @@ import ProductGallery from 'src/components/sections/ProductGallery'
 import { ITEMS_PER_PAGE } from 'src/constants'
 import { SearchProvider } from 'src/sdk/search/Provider'
 import type { SearchParamsState } from '@vtex/store-sdk'
+import { Skeleton } from '@vtex/store-ui'
 import type { Props as PageProps } from 'src/pages/{StoreCollection.slug}/[...]'
 
 import { useCollection } from './hooks/useCollection'
 import Seo from './Seo'
+
+import 'src/styles/skeleton.css'
 
 interface Props extends PageProps {
   searchParams: SearchParamsState
@@ -32,35 +35,36 @@ function View(props: Props) {
   return (
     <>
       <h1 className="absolute top-[-100px]">{title}</h1>
+      <div className="py-8 w-11/12 m-auto">
+        {search == null || site == null || storeCollection == null ? (
+          <Skeleton />
+        ) : (
+          <SearchProvider
+            searchParams={searchParams}
+            pageInfo={{
+              size: ITEMS_PER_PAGE,
+              total: Math.ceil(
+                search.products.pageInfo.totalCount / ITEMS_PER_PAGE
+              ),
+            }}
+          >
+            {/* TODO: Move seo components to SSG */}
+            <Seo
+              title={title}
+              slug={slug}
+              site={site}
+              storeCollection={storeCollection}
+            />
 
-      {search == null || site == null || storeCollection == null ? (
-        <div>...loading</div>
-      ) : (
-        <SearchProvider
-          searchParams={searchParams}
-          pageInfo={{
-            size: ITEMS_PER_PAGE,
-            total: Math.ceil(
-              search.products.pageInfo.totalCount / ITEMS_PER_PAGE
-            ),
-          }}
-        >
-          {/* TODO: Move seo components to SSG */}
-          <Seo
-            title={title}
-            slug={slug}
-            site={site}
-            storeCollection={storeCollection}
-          />
-
-          {/* UI components */}
-          <ProductGallery
-            fallbackData={dynamicData}
-            products={search.products}
-            facets={search.facets}
-          />
-        </SearchProvider>
-      )}
+            {/* UI components */}
+            <ProductGallery
+              fallbackData={dynamicData}
+              products={search.products}
+              facets={search.facets}
+            />
+          </SearchProvider>
+        )}
+      </div>
     </>
   )
 }
